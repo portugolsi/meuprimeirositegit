6d5eac4b4bd4a4de112b1b3f2e08d964d2a0abce
portugolsi <jonadabe.santana@dcx.ufpb.br>
2024-04-12 17:32:50 -0300

    Alterações no CSS

--- projeto/src/components/card.js
+++ projeto/src/components/card.js
@@ -2,13 +2,24 @@ import React from "react";
 import './styles/card.css'
 
 export default function Card(){
-
     return(
+        <div className="card" >            
+            <div className="foto">
 
-        <>
-        <h1>Esse será um card</h1>
-        </>
+                <img id="foto" src="https://rickandmortyapi.com/api/character/avatar/65.jpeg" ></img>
 
+            </div>
+
+            <div className="atributos">
+                <h3>Chris</h3>
+                <div id="status">
+                    <span id="bolinha"></span> <span id="status">Alive</span> - <span id="especie">Humanoid</span>
+                </div>
+                Last known location:
+                Rick's Battery Microverse
+                First seen in:
+                The Ricks Must Be Crazy
+            </div>
+        </div>
     )
-
 }
